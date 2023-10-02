5edcc23a0a7a374a77e05399c7ccb67d5cf4574c
KevinDinhDev <kevindinhdev@gmail.com>
2023-10-02 16:04:23 -0400

    Adding Classes and interface

---
+++ SpacedLogger.java
@@ -0,0 +1,25 @@
+package week05;
+
+public class SpacedLogger implements Logger{
+
+	@Override
+	public void log(String log) {
+		StringBuilder spacedLog = new StringBuilder();
+		for (int i = 0; i < log.length(); i++) {
+			spacedLog.append(log.charAt(i)).append(" ");
+		}
+		System.out.println(spacedLog.toString());
+		
+	}
+
+	@Override
+	public void error(String error) {
+		StringBuilder spacedError = new StringBuilder();
+		for (int i = 0; i < error.length(); i++) {
+			spacedError.append(error.charAt(i)).append(" ");
+		}
+		System.out.println("ERROR: " + spacedError.toString());
+		
+	}
+	
+}
